4b233761b012a85e7a3d49f55d86d96e24f47576
hizkia2382008 <2382008@unai.edu>
2024-09-12 13:34:18 +0700

    Handson2

--- soal1.java
+++ soal1.java
@@ -1,12 +1,14 @@
+import java.util.Scanner;
+
 public class soal1 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner();
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Masukkan nilai 1: ");
-        int nilai1 = input.nextInt();
+        int nilai1 = scanner.nextInt();
 
         System.out.println("Masukkan nilai 2: ");
-        int nilai2 = input.nextIn();
+        int nilai2 = scanner.nextInt();
 
         int nilaiTertinggi = (nilai1 > nilai2) ? nilai1:nilai2;
 
